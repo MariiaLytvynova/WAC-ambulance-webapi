fee1fff04607e689ec31c44b36521c39c3abf627
lytvynovamariia <litmarrom@gmail.com>
2026-06-13 03:55:21 +0200

    Add postgres conf file

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -10,10 +10,26 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:AMBULANCE_API_ENVIRONMENT="Development"
 $env:AMBULANCE_API_PORT="8080"
+$env:AMBULANCE_API_DB_USERNAME="root"
+$env:AMBULANCE_API_DB_PASSWORD="neUhaDnes"
+
+function postgres {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
+
+
 
 switch ($command) {
     "start" {
-        go run ${ProjectRoot}/cmd/ambulance-api-service
+        try{
+            postgres up --detach
+            go run ${ProjectRoot}/cmd/ambulance-api-service
+        } finally {
+            postgres down
+        }
+    }
+        "postgres" {
+        postgres up
     }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
