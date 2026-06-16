f0ec5a0c8b13c2a1fe2e96f025bc9c4cacdadcdb
lytvynovamariia <litmarrom@gmail.com>
2026-06-16 07:29:07 +0200

    b

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -2,41 +2,65 @@ param (
     $command
 )
 
-if (-not $command)  {
+if (-not $command) {
     $command = "start"
 }
 
 $ProjectRoot = "${PSScriptRoot}/.."
 
-$env:AMBULANCE_API_ENVIRONMENT="Development"
-$env:AMBULANCE_API_PORT="8080"
-$env:AMBULANCE_API_DB_USERNAME="root"
-$env:AMBULANCE_API_DB_PASSWORD="neUhaDnes"
+# ----------------------------
+# Environment variables (LOCAL DEV)
+# ----------------------------
+$env:AMBULANCE_API_ENVIRONMENT = "Development"
+$env:AMBULANCE_API_PORT = "8080"
 
+$env:POSTGRES_HOST = "localhost"
+$env:POSTGRES_PORT = "5432"
+$env:POSTGRES_USER = "root"
+$env:POSTGRES_PASSWORD = "neUhaDnes"
+$env:POSTGRES_DB = "ambulance"
+$env:POSTGRES_SSLMODE = "disable"
+
+# ----------------------------
+# Docker compose helper
+# ----------------------------
 function postgres {
     docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
 }
 
-
-
+# ----------------------------
+# Commands
+# ----------------------------
 switch ($command) {
+
     "start" {
-        try{
+        try {
             postgres up --detach
+
             go run ${ProjectRoot}/cmd/ambulance-api-service
+
         } finally {
             postgres down
         }
     }
-        "postgres" {
+
+    "postgres" {
         postgres up
     }
+
     "openapi" {
-        docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+        docker run --rm -ti `
+            -v ${ProjectRoot}:/local `
+            openapitools/openapi-generator-cli `
+            generate -c /local/scripts/generator-cfg.yaml
     }
+
     "docker" {
-         docker build -t mariia610/ambulance-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
-   }
+        docker build `
+            -t mariia610/ambulance-webapi:local-build `
+            -f ${ProjectRoot}/build/docker/Dockerfile .
+    }
+
     default {
         throw "Unknown command: $command"
     }
